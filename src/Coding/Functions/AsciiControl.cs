552bb0c8a62565a07bc938a3a82732fdfa7170eb
DiamondCoder02 <diamond.coder.backup@gmail.com>
2024-03-13 12:43:55 +0100

    -

---
+++ src/Coding/Functions/AsciiControl.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CandyBoxCS.src.Coding.Functions
+{
+    internal class AsciiControl
+    {
+        public static string AsciiAnimation(string location)
+        {
+            string imageAscii = "";
+
+            return imageAscii;
+        }
+    }
+}
